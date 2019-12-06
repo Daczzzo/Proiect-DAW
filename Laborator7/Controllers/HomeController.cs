33a05c0b3eb4baef93119be51f5a899690a687ae
Florentina Gusoi <gusoi.florentina@yahoo.com>
2019-12-06 17:46:44 +0200

    added a comment

--- Laborator7/Controllers/HomeController.cs
+++ Laborator7/Controllers/HomeController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+//comm
+//123
 namespace Laborator7.Controllers
 {
     public class HomeController : Controller
